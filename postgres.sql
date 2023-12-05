808dd29c7b791e424635e4f8bcd6971c207b0b60
Peter Larin <petr.m.larin@gmail.com>
2023-12-06 00:24:26 +0300

    update postgres

--- postgres.sql
+++ postgres.sql
@@ -40,6 +40,50 @@ drop column mta_tax,
 drop column tolls,
 drop column impr;
 
-select * from taxi limit 10;
+select count(*) from taxi where dist=0;-- order by pickup_dt desc;-- limit 1000;
 
 select count (*) from taxi;
+
+with sss as(
+select 
+  date(pickup_dt) as pickup_dt,
+  --n_pass,
+--  case
+--    when n_pass > 3 then 4
+--	else n_pass
+--  end as n_p,
+  sum(
+  case
+    when n_pass = 0 then 1
+    else 0
+  end) as n_0,
+  sum(
+  case
+    when n_pass = 1 then 1
+    else 0
+  end) as n_1,
+  sum(
+  case
+    when n_pass = 2 then 1
+    else 0
+  end) as n_2,
+  sum(
+  case
+    when n_pass = 3 then 1
+    else 0
+  end) as n_3,
+  sum(
+  case
+    when n_pass > 3 then 1
+    else 0
+  end) as n_4,
+  max(
+  case
+    when n_pass = 3 then total
+    else NULL
+  end) as max_3
+from taxi
+group by date(pickup_dt)--, n_p
+order by date(pickup_dt)
+
+)select *, round(n_3*100.0/(n_0 + n_1 + n_2 + n_3 + n_4),2) as p_3 from sss
