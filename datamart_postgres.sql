9ff16057faf13aee6f385b869943aab78d3c7d89
Peter Larin <petr.m.larin@gmail.com>
2023-12-07 14:39:20 +0300

    refactor

--- datamart_postgres.sql
+++ datamart_postgres.sql
@@ -1,5 +1,5 @@
+-- Создаем таблицу для импорта исходных данных:
 drop table if exists taxi;
-
 create table taxi(
   v_id int,
   pickup_dt timestamp,
@@ -21,14 +21,21 @@ create table taxi(
   cong float
 );
 
+-- Импортируем и фильтруем данные:
 copy public.taxi
 from '/input_data/yellow_tripdata_2020-01.csv'
 (format csv, header)
-where v_id is not null                   -- Неинформативные строки с 5 NULL в каждой, включая кол-во пассажиров.
-  and date(pickup_dt) <= '2020-01-31'    -- Поездка либо начинается либо заканчивается 
-  and date(dropoff_dt) >= '2020-01-01'   --   в январе 2020 либо и то и другое.
-  and total > 0;                         -- Отсеиваем "странные" поездки со стоимостью <= 0; можно
-                                         --   этого не делать, но тогда колонки MIN неинформативны.                    
+where 
+  -- Неинформативные строки с 5 NULL в каждой, включая кол-во пассажиров:
+  v_id is not null
+  -- Поездка либо начинается либо заканчивается в январе 2020 либо и то и другое:
+  and date(pickup_dt) <= '2020-01-31'
+  and date(dropoff_dt) >= '2020-01-01'
+  -- Отсеиваем "странные" поездки со стоимостью <= 0; можно этого не делать, но тогда
+  -- колонки min неинформативны. (Хотя в итоге они все равно получились малоинформативными).
+  and total > 0;
+                         
+-- Избавляемся от ненужных столбцов:
 alter table taxi
 drop column v_id,
 drop column dropoff_dt,
@@ -40,14 +47,18 @@ drop column paymt,
 drop column fare,
 drop column extra,
 drop column mta_tax,
+drop column tip,
 drop column tolls,
 drop column impr,
 drop column cong;
 
+-- Создаем итоговую таблицу и витрину:
 drop table if exists data_mart;
 create table data_mart as
+-- Одним запросом сделать не получается, нужна промежуточная таблица (CTE). Если бы были нужны
+-- не проценты, а абсолютные значения, можно было бы обойтись без промежуточного CTE:
 with raw as(
-select 
+select
   date(pickup_dt) as pickup_dt,
   sum(case when n_pass = 0 then 1 else 0 end) as n_0,
   sum(case when n_pass = 1 then 1 else 0 end) as n_1,
@@ -67,6 +78,7 @@ select
 from taxi
 group by date(pickup_dt)
 )
+-- Второй запрос, нужен для расчета процента:
 select
   date(pickup_dt) as pickup_date,
   round(n_0 * 100.0/(n_0 + n_1 + n_2 + n_3 + n_4), 2) as per_0p,
@@ -74,12 +86,15 @@ select
   round(n_2 * 100.0/(n_0 + n_1 + n_2 + n_3 + n_4), 2) as per_2p,
   round(n_3 * 100.0/(n_0 + n_1 + n_2 + n_3 + n_4), 2) as per_3p,
   round(n_4 * 100.0/(n_0 + n_1 + n_2 + n_3 + n_4), 2) as "per_4p+",
-  min_0p, max_0p, min_1p, max_1p, min_2p, max_2p, min_3p, max_3p, min_4p as "min_4p+", max_4p as "max_4p+"
+  min_0p, max_0p, min_1p, max_1p, min_2p, max_2p, min_3p, max_3p,
+  min_4p as "min_4p+", max_4p as "max_4p+"
 from raw
 order by pickup_date;
 
+-- Сохраняем в csv. Postgres не поддерживает parquet?
 copy data_mart
-to '/output_data_pg/result_pg.csv'
+to '/output_data_postgres/datamart_postgres.csv'
 (format csv, header);
 
+-- Показать витрину:
 select * from data_mart;
